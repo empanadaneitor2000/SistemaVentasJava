a3e2161d1eddb05545d0def4dac90b5c45ea972a
empanadaneitor2000 <andresfelipemj123@gmail.com>
2022-09-29 10:50:59 -0500

    clienteokay

--- src/Vistas/Clientesform.java
+++ src/Vistas/Clientesform.java
@@ -9,7 +9,7 @@ import javax.swing.table.DefaultTableModel;
 
 
 public class Clientesform extends javax.swing.JInternalFrame {
-
+    int idProducto;
     ClienteDAO dao = new ClienteDAO();
     Cliente cl = new Cliente();
     DefaultTableModel modelo = new DefaultTableModel();
@@ -57,11 +57,9 @@ public class Clientesform extends javax.swing.JInternalFrame {
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox();
-        jComboBox2 = new javax.swing.JComboBox();
-        btnadd = new javax.swing.JButton();
+        cbotipdoc = new javax.swing.JComboBox();
+        cboestado = new javax.swing.JComboBox();
         btnupd = new javax.swing.JButton();
-        btndelete = new javax.swing.JButton();
         btnnew = new javax.swing.JButton();
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
@@ -88,33 +86,19 @@ public class Clientesform extends javax.swing.JInternalFrame {
 
         jLabel5.setText("ESTADO");
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "SELECCIONAR", "1", "2" }));
-        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+        cbotipdoc.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "SELECCIONAR", "1", "2" }));
+        cbotipdoc.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jComboBox1ActionPerformed(evt);
+                cbotipdocActionPerformed(evt);
             }
         });
 
-        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "SELECCIONAR", "1", "2" }));
-
-        btnadd.setText("AGREGAR");
-        btnadd.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnaddActionPerformed(evt);
-            }
-        });
+        cboestado.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "SELECCIONAR", "1", "2" }));
 
         btnupd.setText("ACTUALIZAR");
         btnupd.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnupdActionPerformed(evt);
-            }
-        });
-
-        btndelete.setText("ELIMINAR");
-        btndelete.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btndeleteActionPerformed(evt);
             }
         });
 
@@ -144,6 +128,11 @@ public class Clientesform extends javax.swing.JInternalFrame {
                 "id", "Nombres", "ima", "Apellidos", "Fecha de nacimiento", "Documento", "Estado", "Tipo de documento"
             }
         ));
+        tabla.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tablaMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(tabla);
 
         jLabel7.setText("COD-USUARIO");
@@ -170,11 +159,7 @@ public class Clientesform extends javax.swing.JInternalFrame {
                     .addComponent(txtapellido, javax.swing.GroupLayout.DEFAULT_SIZE, 172, Short.MAX_VALUE))
                 .addGap(18, 18, 18)
                 .addComponent(btnnew, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 109, Short.MAX_VALUE)
-                .addComponent(btndelete, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btnadd, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(45, 45, 45))
+                .addGap(45, 350, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
@@ -206,30 +191,23 @@ public class Clientesform extends javax.swing.JInternalFrame {
                                 .addGap(51, 51, 51)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(cboestado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                         .addComponent(btnupd))
-                                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                    .addComponent(cbotipdoc, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 742, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(18, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(28, 28, 28)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(btnnew)
-                            .addComponent(txtcod, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(22, 22, 22)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(btnadd)
-                            .addComponent(btndelete))))
+                .addGap(28, 28, 28)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnnew)
+                    .addComponent(txtcod, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(12, 12, 12)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
@@ -259,7 +237,7 @@ public class Clientesform extends javax.swing.JInternalFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(txtestado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(cboestado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGap(13, 13, 13)
@@ -269,7 +247,7 @@ public class Clientesform extends javax.swing.JInternalFrame {
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(txttipodoc, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addComponent(jLabel8))
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(cbotipdoc, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 212, Short.MAX_VALUE)
                 .addContainerGap())
@@ -278,17 +256,9 @@ public class Clientesform extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnaddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnaddActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_btnaddActionPerformed
-
     private void btnupdActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnupdActionPerformed
-        // TODO add your handling code here:
+        actualizar();
     }//GEN-LAST:event_btnupdActionPerformed
-
-    private void btndeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btndeleteActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_btndeleteActionPerformed
 
     private void btnnewActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnnewActionPerformed
         buscarid();
@@ -306,8 +276,9 @@ public class Clientesform extends javax.swing.JInternalFrame {
                 txtapellido.setText(Cliente.getApellidos());
                 txtfecha.setText(Cliente.getFechaNacimiento());
                 txtdoc.setText(Integer.toString(Cliente.getDocumento()));
-                txtestado.setText(Integer.toString(Cliente.getEstado_idEstado()));
-                txttipodoc.setText(Integer.toString(Cliente.getTipo_documento_idDocumento()));
+                
+                cboestado.setSelectedItem(Integer.toString(Cliente.getEstado_idEstado()));
+                cbotipdoc.setSelectedItem(Integer.toString(Cliente.getTipo_documento_idDocumento()));
                
             }else{
                JOptionPane.showMessageDialog(this, "Producto no registrado");
@@ -315,23 +286,73 @@ public class Clientesform extends javax.swing.JInternalFrame {
             }
         }
     }
+     void actualizar(){
+          int fila = tabla.getSelectedRow();
+        if (fila == -1) {
+            JOptionPane.showMessageDialog(this, "Seleccionar fila ");
+            
+        }else{
+             String nombre = txtnombre.getText();
+            String apellido = txtapellido.getText();
+            String fecha = txtfecha.getText();
+            String ndoc = txtdoc.getText();
+            String es = cboestado.getSelectedItem().toString();
+            String tdoc = cbotipdoc.getSelectedItem().toString();
+            
+            
+            
+            
+            Object [] obj = new Object[7];
+            obj [0] = nombre;
+            obj [1] = apellido;
+            obj [2] = fecha;
+            obj [3] = ndoc;
+            obj [4] = es;
+            obj [5] = tdoc;
+            obj [6] = idProducto;
+            dao.actualizar(obj);
+            
+         }
+        }
+           
+   
     /*    }*/    
-    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+    private void cbotipdocActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbotipdocActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jComboBox1ActionPerformed
+    }//GEN-LAST:event_cbotipdocActionPerformed
 
     private void txtestadoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtestadoActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_txtestadoActionPerformed
+
+    private void tablaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tablaMouseClicked
+        int fila = tabla.getSelectedRow();
+        if (fila == -1) {
+            JOptionPane.showMessageDialog(this, "Seleccionar fila ");
+            
+        }else{
+            int idProducto = Integer.parseInt(tabla.getValueAt(fila, 0).toString()) ;
+            String nombre = tabla.getValueAt(fila, 1).toString();
+            String apellido = tabla.getValueAt(fila, 3).toString();
+            String fecha = tabla.getValueAt(fila, 4).toString();
+            String ndoc = tabla.getValueAt(fila, 5).toString();
+            String es = tabla.getValueAt(fila, 6).toString();
+            String tdoc = tabla.getValueAt(fila, 7).toString();
+            txtnombre.setText(nombre);
+            txtapellido.setText(apellido);
+            txtfecha.setText(fecha);
+            txtdoc.setText(ndoc);
+            cboestado.setSelectedItem(es);
+            cbotipdoc.setSelectedItem(tdoc);
+        }
+    }//GEN-LAST:event_tablaMouseClicked
 /*}*/
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnadd;
-    private javax.swing.JButton btndelete;
     private javax.swing.JButton btnnew;
     private javax.swing.JButton btnupd;
-    private javax.swing.JComboBox jComboBox1;
-    private javax.swing.JComboBox jComboBox2;
+    private javax.swing.JComboBox cboestado;
+    private javax.swing.JComboBox cbotipdoc;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
